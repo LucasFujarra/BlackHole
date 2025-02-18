39658c3fec74c0890afdce9ec31f647bc01aa2d9
Lucas Fujarra <73505808+LucasFujarra@users.noreply.github.com>
2025-02-18 20:36:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from pytubefix import YouTube
 #Busca por URL
 def buscar(link):
     try:
-        media = YouTube(link,use_oauth=True,allow_oauth_cache=True)
+        media = YouTube(link)
         return media
     except:
         media = ""
